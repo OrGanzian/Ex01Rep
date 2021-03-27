441da5ad59a9dcaffd2717b38e1981b4ade9609d
Or Ganzian <OrGanzian10@gmail.com>
2021-03-27 12:45:22 +0300

    hello Max added to EX01 project

---
+++ B21 Ex01 Or 318446358 Maxim 320407190/B21_Ex01_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace B21_Ex01_1
+{
+    class Program
+    {
+
+
+       static public void Main()
+        {
+            Console.WriteLine("hello Max");
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+        
+    }
+}
