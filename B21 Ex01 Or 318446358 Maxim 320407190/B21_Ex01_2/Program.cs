c154c4b5f5b4f37cfef73e57ccdcc34a4c3e11a1
Or Ganzian <OrGanzian10@gmail.com>
2021-03-28 14:47:05 +0300

    clock is working, need to fix the even stars

---
+++ B21 Ex01 Or 318446358 Maxim 320407190/B21_Ex01_2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace B21_Ex01_2
+{
+    class Program
+    {
+        static public void Main()
+        {
+            RecursiveClockPrint(0, 5, 1);
+            Console.ReadLine();
+        }
+
+
+        public static void RecursiveClockPrint(int offset, int stars, int inc)
+        {
+
+            if (offset < 0 && inc < 0)
+            {
+                return;
+            }
+
+
+            printLine(offset, stars);
+
+            if (stars == 1)
+            {
+                inc = -1;
+            }
+
+            RecursiveClockPrint(offset + inc, stars - inc, inc);
+
+        }
+
+        public static void printLine(int offset, int stars)
+        {
+            for (int i = 0; i < offset; i++)
+            {
+                Console.Write(" ");
+
+            }
+               
+            // Print stars with a space 
+            for (int i = 0; i < stars; i++)
+            {
+                Console.Write("* ");
+
+            }
+            // Print a new line 
+            Console.WriteLine();
+        }
+    }
+}
